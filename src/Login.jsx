d773077a293257e97e9c96907432907e799eb058
jefryfalah32 <muhammadkhafidfuadi@gmail.com>
2023-07-29 13:33:22 +0700

    validation form

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,9 +5,12 @@ const Login = ({ onLogin }) => {
   // const { login } = useAuth();
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [errorEmail, setErrorEmail] = React.useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const response = await axiosLogin.post("/users/authenticate", {
         email,
@@ -18,13 +21,26 @@ const Login = ({ onLogin }) => {
       localStorage.setItem("email", email);
       onLogin();
     } catch (error) {
-      console.error("Login failed:", error);
+      setErrorMessage(error.response.data.message);
+      console.log(errorMessage);
     }
   };
 
   return (
     <>
       <div className="container">
+        {errorMessage && (
+          <div className="alert alert-dismissible fade show" role="alert">
+            {errorMessage}
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+          </div>
+        )}
+
         <img src="../public/images/line.svg" className="line-bg" alt="line" />
 
         <div className="row justify-content-center">
@@ -60,6 +76,22 @@ const Login = ({ onLogin }) => {
                       placeholder="user@user.com"
                     />
                   </div>
+                  {email === "" ? (
+                    <p>
+                      <small className="label-error">
+                        Email cannot be empty
+                      </small>
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  {email !== "" && !email.includes("@") ? (
+                    <p>
+                      <small className="label-error">Email is not valid</small>
+                    </p>
+                  ) : (
+                    ""
+                  )}
                   <div className="form-group position-relative">
                     <img
                       src="../public/images/lock-icon.svg"
@@ -74,6 +106,24 @@ const Login = ({ onLogin }) => {
                       placeholder="********"
                     />
                   </div>
+                  {password === "" ? (
+                    <p>
+                      <small className="label-error">
+                        Password cannot be empty
+                      </small>
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  {password !== "" && password.length < 6 ? (
+                    <p>
+                      <small className="label-error">
+                        Password must be at least 6 characters
+                      </small>
+                    </p>
+                  ) : (
+                    ""
+                  )}
                   <button onClick={handleSubmit} className="btn">
                     Masuk Sekarang
                   </button>
